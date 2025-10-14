5cb13e9eafab38f356cf4add583e503820108582
dongdong-byte <kimeee34@gmail.com>
2025-10-15 01:41:00 +0900

    7단원 마무리 (설명)
    코드 설명은 내일 꼭보기

---
+++ src/main/java/com/example/kim/controller/Ch07/BookMark.java
@@ -0,0 +1,7 @@
+package com.example.kim.controller.Ch07;
+
+public class BookMark {
+
+    public String name;
+    public String url;
+}
---
+++ src/main/java/com/example/kim/controller/Ch07/BookmarkAjaxRestController.java
@@ -0,0 +1,27 @@
+package com.example.kim.controller.Ch07;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@RestController
+public class BookmarkAjaxRestController {
+    private List<BookMark> bookmarks = new ArrayList<>();
+
+
+    @RequestMapping(method = RequestMethod.POST, path = "/bookmark")
+    public String registerBookmark(@RequestBody BookMark bookmark) {
+        bookmarks.add(bookmark);
+        return "registerBookmark";
+    }
+
+    @RequestMapping(method = RequestMethod.GET, path = "/bookmarks")
+    public List<BookMark> getBookmarks() {
+return bookmarks;
+    }
+}
+
+
