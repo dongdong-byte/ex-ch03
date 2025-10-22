7456c3e7359f28d2d7f4702ff33154f5aa4a663b
dongdong-byte <kimeee34@gmail.com>
2025-10-22 09:44:42 +0900

    수정

--- src/main/java/kim/Application.java
+++ src/main/java/kim/Application.java
@@ -4,13 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration;
 
-/**
- * Spring Boot 애플리케이션 메인 클래스
- *
- * 설정 정보:
- * - JPA 자동설정 제외 (MyBatis 및 순수 Java 기반 애플리케이션)
- * - 포트: 8090 (application.yml 참조)
- */
+
 @SpringBootApplication(exclude = {
     HibernateJpaAutoConfiguration.class  // JPA 자동설정 제외
 })
