64eaf82081ddc35d4dcf397f5a79cd66ac3a66eb
dongdong-byte <kimeee34@gmail.com>
2025-10-22 09:44:16 +0900

    수정본

--- src/main/java/kim/Application.java
+++ src/main/java/kim/Application.java
@@ -1,14 +1,19 @@
 package kim;
 
-import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration;
 
+/**
+ * Spring Boot 애플리케이션 메인 클래스
+ *
+ * 설정 정보:
+ * - JPA 자동설정 제외 (MyBatis 및 순수 Java 기반 애플리케이션)
+ * - 포트: 8090 (application.yml 참조)
+ */
 @SpringBootApplication(exclude = {
-    HibernateJpaAutoConfiguration.class  // JPA 자동설정 제외 (MyBatis만 사용)
+    HibernateJpaAutoConfiguration.class  // JPA 자동설정 제외
 })
-@MapperScan("kim.mapper")
 public class Application {
 
 	public static void main(String[] args) {
